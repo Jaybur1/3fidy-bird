5cf7858ef9dbef2bed099ff62cf3264793a5995b
Jaybur1 <jayburbyga@gmail.com>
2020-05-27 13:30:19 -0400

    add background and ground images

--- main.lua
+++ main.lua
@@ -12,3 +12,35 @@ local background = love.graphics.newImage('assets/background.png')
 local ground = love.graphics.newImage('assets/ground.png')
 local pipe = love.graphics.newImage('assets/pipe.png')
 local bird = love.graphics.newImage('assets/bird.png')
+
+function love.load() 
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+
+  love.window.setTitle('3Fiddy Bird')
+
+  push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH, WINDOW_HEIGHT, {
+    vsync = true,
+    fullscreen = false,
+    resizable = true, 
+  })
+end
+
+function love.resize(w,h)
+  push:resize(w,h)
+end
+
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit();
+  end
+end
+
+function love.draw()
+  push:start()
+  love.graphics.draw(background, 0,0)
+
+  love.graphics.draw(ground, 0 , VIRTUAL_HEIGHT - 16)
+
+
+  push:finish()
+end
