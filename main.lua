ae4528a32f32c86366be6c090129bdffd4f2743d
Jaybur1 <jayburbyga@gmail.com>
2020-05-28 14:40:00 -0400

    refactor main file for states

--- main.lua
+++ main.lua
@@ -1,131 +1,133 @@
-push = require "push"
+--[[
+    GD50 2018
+    Flappy Bird Remake
 
-Class = require "class"
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
 
-require "Bird"
-require "Pipe"
-require "PipePair"
+    A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple 
+    but effective gameplay mechanic of avoiding pipes indefinitely by just tapping 
+    the screen, making the player's bird avatar flap its wings and move upwards slightly. 
+    A variant of popular games like "Helicopter Game" that floated around the internet
+    for years prior. Illustrates some of the most basic procedural generation of game
+    levels possible as by having pipes stick out of the ground by varying amounts, acting
+    as an infinitely generated obstacle course for the player.
+]]
 
---game states
+-- push is a library that will allow us to draw our game at a virtual
+-- resolution, instead of however large our window is; used to provide
+-- a more retro aesthetic
+--
+-- https://github.com/Ulydev/push
+push = require 'push'
 
-require "StateMachine"
-require "states/BaseState"
-require "states/PlayState"
-require "states/TitleScreenState"
+-- the "Class" library we're using will allow us to represent anything in
+-- our game as code, rather than keeping track of many disparate variables and
+-- methods
+--
+-- https://github.com/vrld/hump/blob/master/class.lua
+Class = require 'class'
 
+-- a basic StateMachine class which will allow us to transition to and from
+-- game states smoothly and avoid monolithic code in one file
+require 'StateMachine'
+
+-- all states our StateMachine can transition between
+require 'states/BaseState'
+require 'states/CountdownState'
+require 'states/PlayState'
+require 'states/ScoreState'
+require 'states/TitleScreenState'
+
+require 'Bird'
+require 'Pipe'
+require 'PipePair'
+
+-- physical screen dimensions
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+-- virtual resolution dimensions
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
---init Bird
-local bird = Bird()
--- init Pipes Pairs
-local pipePairs = {}
 
-local spawnTimer = 0
--- Load Images
-
---handle background
-local background = love.graphics.newImage("assets/background.png")
+local background = love.graphics.newImage('assets/background.png')
 local backgroundScroll = 0
-local BACKGROUND_SPEED = 30
-local BACKGROUND_LOOPING_POINT = 413
---handle ground
-local ground = love.graphics.newImage("assets/ground.png")
+
+local ground = love.graphics.newImage('assets/ground.png')
 local groundScroll = 0
-local GROUND_SPEED = 60
-local GROUND_LOOPING_POINT = 514
 
--- initialize our last recorded Y value for a gap placement to base other gaps off of
-local lastY = -PIPE_HEIGHT + math.random(80) + 20
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
 
--- scrolling variable to pause the game when we collide with a pipe
-local scrolling = false
-
--- local pipe = love.graphics.newImage("assets/pipe.png")
--- local bird = love.graphics.newImage("assets/bird.png")
--- local fiddy = love.graphics.newImage("assets/3fiddy.png")
+local BACKGROUND_LOOPING_POINT = 413
 
 function love.load()
-  love.graphics.setDefaultFilter("nearest", "nearest")
-  math.randomseed(os.time())
+    -- initialize our nearest-neighbor filter
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    
+    -- seed the RNG
+    math.randomseed(os.time())
 
-  love.window.setTitle("3Fiddy Bird")
+    -- app window title
+    love.window.setTitle('Fifty Bird')
 
-  smallFont = love.graphics.newFont("assets/font.ttf", 12)
-  mediumFont = love.graphics.newFont("assets.flappy.ttf", 14)
-  titleFont = love.graphics.newFont("assets/flappy.ttf", 24)
-  hugeFont = love.graphics.newFont("assets/flappy.ttf", 58)
+    -- initialize our nice-looking retro text fonts
+    smallFont = love.graphics.newFont('assets/font.ttf', 8)
+    mediumFont = love.graphics.newFont('assets/flappy.ttf', 14)
+    flappyFont = love.graphics.newFont('assets/flappy.ttf', 28)
+    hugeFont = love.graphics.newFont('assets/flappy.ttf', 56)
+    love.graphics.setFont(flappyFont)
 
-  sounds = {
-    ["jump"] = love.audio.newSource("assets/jump.wav", "static"),
-    ["explosion"] = love.audio.newSource("assets/explosion.wav", "static"),
-    ["hurt"] = love.audio.newSource("assets/hurt.wav", "static"),
-    ["score"] = love.audio.newSource("assets/score.wav", "static"),
-    ["music"] = love.audio.newSource("assets/marios_way.mp3", "static")
-  }
+    -- initialize our table of sounds
+    sounds = {
+        ['jump'] = love.audio.newSource('assets/jump.wav', 'static'),
+        ['explosion'] = love.audio.newSource('assets/explosion.wav', 'static'),
+        ['hurt'] = love.audio.newSource('assets/hurt.wav', 'static'),
+        ['score'] = love.audio.newSource('assets/score.wav', 'static'),
 
-  -- kick off music
-  sounds["music"]:setLooping(true)
-  sounds["music"]:play()
-
-  text = "start"
-  --screen setup
-  push:setupScreen(
-    VIRTUAL_WIDTH,
-    VIRTUAL_HEIGHT,
-    WINDOW_WIDTH,
-    WINDOW_HEIGHT,
-    {
-      vsync = true,
-      fullscreen = false,
-      resizable = true
+        -- https://freesound.org/people/xsgianni/sounds/388079/
+        ['music'] = love.audio.newSource('assets/marios_way.mp3', 'static')
     }
-  )
 
-  -- initialize state machine with all state-returning functions
-  gStateMachine =
-    StateMachine {
-    ["title"] = function()
-      return TitleScreenState()
-    end,
-    ["countdown"] = function()
-      return CountdownState()
-    end,
-    ["play"] = function()
-      return PlayState()
-    end,
-    ["score"] = function()
-      return ScoreState()
-    end
-  }
-  gStateMachine:change("title")
+    -- kick off music
+    sounds['music']:setLooping(true)
+    sounds['music']:play()
 
-  love.keyboard.keyPressed = {}
-  -- initialize mouse input table
-  love.mouse.buttonsPressed = {}
+    -- initialize our virtual resolution
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+
+    -- initialize state machine with all state-returning functions
+    gStateMachine = StateMachine {
+        ['title'] = function() return TitleScreenState() end,
+        ['countdown'] = function() return CountdownState() end,
+        ['play'] = function() return PlayState() end,
+        ['score'] = function() return ScoreState() end
+    }
+    gStateMachine:change('title')
+
+    -- initialize input table
+    love.keyboard.keysPressed = {}
+
+    -- initialize mouse input table
+    love.mouse.buttonsPressed = {}
 end
 
 function love.resize(w, h)
-  push:resize(w, h)
+    push:resize(w, h)
 end
 
 function love.keypressed(key)
-  love.keyboard.keyPressed[key] = true
+    -- add to our table of keys pressed this frame
+    love.keyboard.keysPressed[key] = true
 
-  if key == "escape" then
-    love.event.quit()
-  end
-
-  if key == "enter" or key == "return" then
-    if scrolling then
-      text = "resume"
-      scrolling = false
-    else
-      scrolling = true
+    if key == 'escape' then
+        love.event.quit()
     end
-  end
 end
 
 --[[
@@ -133,55 +135,41 @@ end
     X and Y of the mouse, as well as the button in question.
 ]]
 function love.mousepressed(x, y, button)
-  love.mouse.buttonsPressed[button] = true
-end
-
-function love.keyboard.wasPressed(key)
-  return love.keyboard.keysPressed[key]
+    love.mouse.buttonsPressed[button] = true
 end
 
 --[[
-  Equivalent to our keyboard function from before, but for the mouse buttons.
+    Custom function to extend LÖVE's input handling; returns whether a given
+    key was set to true in our input table this frame.
 ]]
-function love.mouse.wasPressed(button)
-  return love.mouse.buttonsPressed[button]
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
 end
 
-function love.keyboard.wasPressed(key)
-  if love.keyboard.keyPressed[key] then
-    return true
-  else
-    return false
-  end
+--[[
+    Equivalent to our keyboard function from before, but for the mouse buttons.
+]]
+function love.mouse.wasPressed(button)
+    return love.mouse.buttonsPressed[button]
 end
 
 function love.update(dt)
-  if scrolling then
-    -- scroll background by preset speed * dt, looping back to 0 after the looping point
-    backgroundScroll = (backgroundScroll + BACKGROUND_SPEED * dt) % BACKGROUND_LOOPING_POINT
+    -- scroll our background and ground, looping back to 0 after a certain amount
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
-    -- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
-    groundScroll = (groundScroll + GROUND_SPEED * dt) % GROUND_LOOPING_POINT
-  end
+    gStateMachine:update(dt)
 
-  gStateMachine:update(dt)
-
-  love.keyboard.keysPressed = {}
-  love.mouse.buttonsPressed = {}
+    love.keyboard.keysPressed = {}
+    love.mouse.buttonsPressed = {}
 end
 
 function love.draw()
-  push:start()
-  love.graphics.draw(background, -backgroundScroll, 0)
-  gStateMachine:render()
-  love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
-
-  push:finish()
-end
-
-function displayTitle(text)
-  love.graphics.setFont(titleFont)
-  love.graphics.printf("3-Fiddy bird", 0, 50, VIRTUAL_WIDTH, "center")
-  love.graphics.setFont(smallFont)
-  love.graphics.printf("Press ENTER to " .. text, 0, 80, VIRTUAL_WIDTH, "center")
+    push:start()
+    
+    love.graphics.draw(background, -backgroundScroll, 0)
+    gStateMachine:render()
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+    
+    push:finish()
 end
