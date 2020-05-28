8d47602a1f01e7fbb3a1dd5d9202bb1b62374099
Jaybur1 <jayburbyga@gmail.com>
2020-05-28 16:09:33 -0400

    add rendomize gap between pipes

--- states/PlayState.lua
+++ states/PlayState.lua
@@ -15,6 +15,8 @@ function PlayState:init()
 
     -- initialize our last recorded Y value for a gap placement to base other gaps off of
     self.lastY = -PIPE_HEIGHT + math.random(80) + 20
+
+    self.timerGap = 2
 end
 
 function PlayState:update(dt)
@@ -22,7 +24,7 @@ function PlayState:update(dt)
     self.timer = self.timer + dt
 
     -- spawn a new pipe pair every second and a half
-    if self.timer > 2 then
+    if self.timer >  math.floor( self.timerGap )then
         -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
         -- no higher than 10 pixels below the top edge of the screen,
         -- and no lower than a gap length (90 pixels) from the bottom
@@ -35,6 +37,7 @@ function PlayState:update(dt)
 
         -- reset timer
         self.timer = 0
+        self.timerGap = math.random(2,5)
     end
 
     -- for every pair of pipes..
